4c0dcf4deb7bc55d21204d2ac64511e3ef8b576d
mmmdbybyd <915445800@qq.com>
2017-06-04 20:23:14 -0500

    Update build_server.sh

--- build_server.sh
+++ build_server.sh
@@ -8,15 +8,15 @@ done
 read -p "please input upper ip(default is 114.114.114.114): " upperip
 
 cd
-yum -y update && PM=yum || apt-get -y update && PM=apt-get || exec echo "not support OS."
+yum -y update && PM=yum || apt-get -y update && PM=apt-get
 $PM -y install gcc curl psmisc
 curl -O -k https://raw.githubusercontent.com/mmmdbybyd/httpDNS/master/http-dns-server.c || \
-exec echo "download source code failed."
-gcc -o httpdns -O3 http-dns-server.c || exec echo "compiled failed."
+exec echo -e "\033[41;37mdownload source code failed.\033[0m"
+gcc -o httpdns -O3 http-dns-server.c || exec echo "\033[41;37mcompiled failed.\033[0m"
 strip httpdns
 rm -f http-dns-server.c
 ./httpdns -l $server_port -u ${upperip:=114.114.114.114} &>/dev/null || \
-exec echo "httpdns is stoped"
+exec echo "\033[41;37mhttpdns is stoped\033[0m"
 echo "
 while true
 do
@@ -33,3 +33,4 @@ done
 " >updatehosts.sh
 chmod +x updatehosts.sh
 nohup ./updatehosts.sh &>/dev/null &
+echo -e "\033[34mhttpdns is running.\033[0m\n"
